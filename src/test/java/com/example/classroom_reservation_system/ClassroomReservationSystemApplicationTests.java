05c61d0028c8f46fc7c9521300563051e24a0538
Saangjun00 <tkdwnsdl0918@gamil.com>
2025-04-15 22:40:41 +0900

    initial commit

---
+++ src/test/java/com/example/classroom_reservation_system/ClassroomReservationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.classroom_reservation_system;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassroomReservationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
